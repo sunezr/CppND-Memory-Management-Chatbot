d827b3c470f231898738f1cf4e04a5ddb31f2fbf
frank <franksunus@gmail.com>
2020-12-22 17:00:29 +0800

    Task 5

--- src/graphnode.h
+++ src/graphnode.h
@@ -22,7 +22,8 @@ private:
     // data handles (not owned)
     std::vector<std::unique_ptr<GraphEdge>> _childEdges; // edges to subsequent nodes
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes
-    ChatBot *_chatBot;
+    // ChatBot *_chatBot;
+    ChatBot _chatBot;
 
     ////
     //// EOF STUDENT CODE
@@ -46,13 +47,14 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    // void AddEdgeToChildNode(GraphEdge *edge);std::unique_ptr<GraphEdge>
+    // void AddEdgeToChildNode(GraphEdge *edge);
     void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
 
-    void MoveChatbotHere(ChatBot *chatbot);
+    // void MoveChatbotHere(ChatBot *chatbot);
+    void MoveChatbotHere(ChatBot chatbot);
 
     ////
     //// EOF STUDENT CODE
